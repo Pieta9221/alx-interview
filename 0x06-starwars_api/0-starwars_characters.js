316f9fcd77bf71ec464e9a74a87401203da7ad12
Pieta9221 <101437355+Pieta9221@users.noreply.github.com>
2023-09-21 14:32:45 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,27 @@
 #!/usr/bin/node
 
-const axios = require('axios');
+const request = require('request');
 
-const movieId = process.argv[2];
-
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-axios.get(url)
-  .then(async (response) => {
-    const charactersArray = response.data.characters;
-    for (const character of charactersArray) {
-      try {
-        const characterResponse = await axios.get(character);
-        console.log(characterResponse.data.name);
-      } catch (error) {
-        console.error(error);
-      }
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
     }
-  })
-  .catch((error) => {
-    console.error(error);
   });
+};
+
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
+  }
+);
